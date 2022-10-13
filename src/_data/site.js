225efd9edc3f1c7b03798a1b0d2df4e70173a4f5
Jérôme Coupé <jercoupe@gmail.com>
2022-10-13 21:18:30 +0200

    update meta

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,9 @@
 module.exports = {
-  title: "Soirée MM 2022",
-  description: "Soirée MM IAD",
-  url: "https://iad-metiers-mm.netlify.app",
-  metaImage: "https://iad-metiers-mm/assets/img/socialmedia.jpg",
+  title: "Réalisation Multimédia: Soirée Métiers 2022",
+  description:
+    "Cinq anciens multimédias partagent avec vous leurs parcours, leurs joies et leurs peines",
+  url: "https://metiers.iadmm.be/",
+  metaImage: "https://metiers.iadmm.be/assets/img/socialmedia.jpg",
   authorName: "IAD MM",
   authorTwitter: "@jeromecoupe",
   buildDate: new Date(),
